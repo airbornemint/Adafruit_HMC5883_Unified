9584266ea2207964b10ac7a1e3be6d2270cb1423
Ben Artin <ben@artins.org>
2017-03-29 05:09:21 -0400

    Fixed compile error in example

--- examples/magsensor/magsensor.ino
+++ examples/magsensor/magsensor.ino
@@ -3,7 +3,7 @@
 
   Designed specifically to work with the Adafruit HMC5883 Breakout
   http://www.adafruit.com/products/1746
- 
+
   *** You will also need to install the Adafruit_Sensor library! ***
 
   These displays use I2C to communicate, 2 pins are required to interface.
@@ -14,11 +14,11 @@
 
   Written by Kevin Townsend for Adafruit Industries with some heading example from
   Love Electronics (loveelectronics.co.uk)
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the version 3 GNU General Public License as
  published by the Free Software Foundation.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -31,7 +31,7 @@
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_HMC5883_U.h>
+#include <Adafruit_HMC5883.h>
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
@@ -46,17 +46,17 @@ void displaySensorDetails(void)
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");
   Serial.println("------------------------------------");
   Serial.println("");
   delay(500);
 }
 
-void setup(void) 
+void setup(void)
 {
   Serial.begin(9600);
   Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
-  
+
   /* Initialise the sensor */
   if(!mag.begin())
   {
@@ -64,17 +64,17 @@ void setup(void)
     Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
     while(1);
   }
-  
+
   /* Display some basic information on this sensor */
   displaySensorDetails();
 }
 
-void loop(void) 
+void loop(void)
 {
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
+  /* Get a new sensor event */
+  sensors_event_t event;
   mag.getEvent(&event);
- 
+
   /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
   Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
   Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
@@ -83,26 +83,26 @@ void loop(void)
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
   float heading = atan2(event.magnetic.y, event.magnetic.x);
-  
+
   // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
   // Find yours here: http://www.magnetic-declination.com/
   // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
   // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
   float declinationAngle = 0.22;
   heading += declinationAngle;
-  
+
   // Correct for when signs are reversed.
   if(heading < 0)
     heading += 2*PI;
-    
+
   // Check for wrap due to addition of declination.
   if(heading > 2*PI)
     heading -= 2*PI;
-   
+
   // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  
+  float headingDegrees = heading * 180/M_PI;
+
   Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
-  
+
   delay(500);
 }
